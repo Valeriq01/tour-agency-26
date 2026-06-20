a6e4c7d50145e6975e74e9f590c98daf05a082f6
Valeriq01 <valeriyakarapavlova@gmail.com>
2026-06-20 12:12:14 +0300

    chore: create initial project

---
+++ src/main/java/tourAgency/tour_agency/TourAgencyApplication.java
@@ -0,0 +1,13 @@
+package tourAgency.tour_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAgencyApplication.class, args);
+	}
+
+}
