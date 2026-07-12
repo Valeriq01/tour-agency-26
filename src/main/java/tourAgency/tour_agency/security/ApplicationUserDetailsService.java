fe989dcada61e0f3513cdb68ea4125e5380b2180
Valeriq01 <valeriyakarapavlova@gmail.com>
2026-07-12 12:05:55 +0300

    feat: integrate Spring Security authentication

---
+++ src/main/java/tourAgency/tour_agency/security/ApplicationUserDetailsService.java
@@ -0,0 +1,30 @@
+package tourAgency.tour_agency.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tourAgency.tour_agency.model.entity.user.User;
+import tourAgency.tour_agency.repository.user.UserRepository;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public ApplicationUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found: " + username));
+
+        return new ApplicationUserDetails(user);
+    }
+}
