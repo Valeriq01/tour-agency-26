25f569caf8b14eda3bee82b67b5b315e22ee7ac9
Valeriq01 <valeriyakarapavlova@gmail.com>
2026-07-27 21:39:36 +0300

    refactor: setup monorepo structure with main app and rest microservice

---
+++ rest-microservice/src/main/java/touragency/tour_reviews/TourReviewsApplication.java
@@ -0,0 +1,13 @@
+package touragency.tour_reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourReviewsApplication.class, args);
+	}
+
+}
---
+++ rest-microservice/src/test/java/touragency/tour_reviews/TourReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package touragency.tour_reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
